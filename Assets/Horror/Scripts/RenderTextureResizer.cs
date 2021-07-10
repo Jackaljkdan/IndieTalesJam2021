c1556603325d64a06ed49d86c8a3e7d4a13c132a
Daniele Cortesi <dani.c93@hotmail.it>
2021-07-10 09:13:32 +0200

    Add render texture

---
+++ Assets/Horror/Scripts/RenderTextureResizer.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Horror
+{
+    [RequireComponent(typeof(RawImage))]
+    public class RenderTextureResizer : MonoBehaviour
+    {
+        #region Inspector
+
+
+
+        #endregion
+
+        [Inject(Id = "player.camera")]
+        private Camera playerCamera = null;
+
+        private void Start()
+        {
+            UpdateRenderTextureSize();
+        }
+
+        private RenderTexture UpdateRenderTextureSize()
+        {
+            var rawImage = GetComponent<RawImage>();
+            RenderTexture renderTexture = rawImage.texture as RenderTexture;
+
+            var copy = new RenderTexture(renderTexture)
+            {
+                height = (int)(renderTexture.width * rawImage.rectTransform.rect.height / rawImage.rectTransform.rect.width),
+            };
+
+            rawImage.texture = copy;
+            playerCamera.targetTexture = copy;
+
+            renderTexture.DiscardContents();
+            renderTexture.Release();
+
+            //Debug.Log($"updating to {copy.width}x{copy.height}");
+
+            return renderTexture;
+        }
+
+        private void OnRectTransformDimensionsChange()
+        {
+            if (playerCamera != null)
+            {
+                RenderTexture oldRenderTexture = UpdateRenderTextureSize();
+                Destroy(oldRenderTexture);
+            }
+        }
+    }
+}
