a105d8ea31fbbe06e391b6810d2354d67132e862
Daniele Cortesi <dani.c93@hotmail.it>
2021-07-10 23:41:41 +0200

    Add radio

---
+++ Assets/Horror/Scripts/Interaction/RadioInteractable.cs
@@ -0,0 +1,46 @@
+using JK.Interaction;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Horror.Interaction
+{
+    public class RadioInteractable : InteractableBehaviour
+    {
+        #region Inspector
+
+        [SerializeField]
+        private AudioSource audioSource = null;
+
+        [SerializeField]
+        private MeshRenderer emissiveButtonRenderer = null;
+
+        #endregion
+
+        protected override void Start()
+        {
+            base.Start();
+            emissiveButtonRenderer.material.DisableKeyword("_EMISSION");
+        }
+
+        protected override void PerformInteraction(RaycastHit hit)
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+                emissiveButtonRenderer.material.DisableKeyword("_EMISSION");
+            }
+            else
+            {
+                audioSource.Play();
+                emissiveButtonRenderer.material.EnableKeyword("_EMISSION");
+            }
+        }
+    }
+    
+    [Serializable]
+    public class UnityEventRadioInteractable : UnityEvent<RadioInteractable> { }
+}
