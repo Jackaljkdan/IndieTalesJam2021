61dfd0485ee1c549fb7c5da7a120b04fdf63fc00
Daniele Cortesi <dani.c93@hotmail.it>
2021-07-10 13:18:54 +0200

    Add lucy's animations

---
+++ Assets/Horror/Scripts/Lucy/LucyJerk.cs
@@ -0,0 +1,45 @@
+using JK.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Horror.Lucy
+{
+    [RequireComponent(typeof(Animator))]
+    public class LucyJerk : MonoBehaviour
+    {
+        #region Inspector
+
+        public float averageJerksPerSecond = 0.2f;
+
+        #endregion
+
+        private void Start()
+        {
+            JerkAndSchedule();
+        }
+
+        public void JerkAndSchedule()
+        {
+            Jerk();
+            float waitSeconds = RandomUtils.Exponential(averageJerksPerSecond);
+            Debug.Log($"seconds until next jerk: {waitSeconds}");
+            Invoke(nameof(JerkAndSchedule), waitSeconds);
+        }
+
+        public void Unschedule()
+        {
+            CancelInvoke(nameof(JerkAndSchedule));
+        }
+
+        private void Jerk()
+        {
+            GetComponent<Animator>().Play("LucyHeadJerk", 1);
+        }
+    }
+    
+    [Serializable]
+    public class UnityEventLucyJerk : UnityEvent<LucyJerk> { }
+}
